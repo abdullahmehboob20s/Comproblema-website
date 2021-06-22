99bd4e6983600c5f94d46c19e6d4d69f37ca2c7c
abdullahmehboob20s <abdullahmehboob2002@gmail.com>
2021-06-22 02:33:53 -0700

    website complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,17 +5,30 @@ let close_header_right = document.querySelector(".close_header_right");
 let header_right_black_screen = document.querySelector(
   ".header_right_black_screen"
 );
+let faq_accordion_body_up = document.querySelectorAll(".faq_accordion_body");
 
 for (let i = 0; i < faq_accordion.length; i++) {
   faq_accordion[i].addEventListener("click", (e) => {
     let faq_accordion_body = faq_accordion[i].querySelector(
       ".faq_accordion_body"
     );
+
+    faq_accordion.forEach((item2) => {
+      if (item2 !== faq_accordion[i]) {
+        let faq_accordion_body_nested = item2.querySelector(
+          ".faq_accordion_body"
+        );
+        faq_accordion_body_nested.style.height = "";
+        item2.classList.remove("active");
+      }
+    });
+
     if (faq_accordion[i].classList.contains("active")) {
       faq_accordion[i].classList.remove("active");
       faq_accordion_body.style.height = "0px";
       return;
     }
+
     faq_accordion[i].classList.add("active");
     faq_accordion_body.style.height = faq_accordion_body.scrollHeight + "px";
   });
